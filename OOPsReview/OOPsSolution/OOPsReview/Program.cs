77ebeefe94ef1c88f8c1b8fa727137da23ecfe3e
zdoucette97 <54864223+zdoucette97@users.noreply.github.com>
2019-09-13 12:56:40 -0600

    console.write

--- OOPsReview/OOPsSolution/OOPsReview/Program.cs
+++ OOPsReview/OOPsSolution/OOPsReview/Program.cs
@@ -70,7 +70,7 @@ namespace OOPsReview
             width = 3.0;
             style = "Neighbour friendly: spruce";
             price = 72.45;
-            GateInfo = new Gate(height, width, style, price);
+            GateInfo = new Gate(height, width, style, price); //greedy constructor
             gatelist.Add(GateInfo);
 
             //create the Estimate 
@@ -81,6 +81,21 @@ namespace OOPsReview
             ClientEstimate.TotalGates = gatelist;
             //ClientEstimate.TotalPrice = 450.98; this cannot be done as the price is private. instead you muse use the public double
             ClientEstimate.CalculateTotalPrice();
+
+            //nothing should be stored in a static class, only behaviours
+            Console.WriteLine("The fence is to be a " + ClientEstimate.TotalPanels.Style + "style"); ///Class.Property.PropertyInstance/Behaviour
+            Console.WriteLine("The linear fence length is {0:0.0}", ClientEstimate.LinearLength); //Placeholder concatenation
+            Console.WriteLine("Number of required panels is {0}", 
+                                ClientEstimate.TotalPanels.EstimatedNumberOfPanels(ClientEstimate.LinearLength)); //Class.Property.Method(Parameters)
+            Console.WriteLine("Number of required gates {0}",
+                                ClientEstimate.TotalGates.Count);
+            double fencearea = ClientEstimate.TotalPanels.FenceArea(ClientEstimate.LinearLength);
+            foreach(var item in ClientEstimate.TotalGates)
+            {
+                fencearea += item.GateArea();
+            }
+            Console.WriteLine(string.Format("Total Fence Surface Area: {0:0.00}", fencearea * 2)); //2 sides of the fence
+            Console.ReadKey(); //required due to using just f5
         }
     }
 }
