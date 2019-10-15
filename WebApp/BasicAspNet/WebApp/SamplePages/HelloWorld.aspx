ac9eb07fcb8a0f91cd8f3e70d2241614ae852a3c
zdoucette97 <54864223+zdoucette97@users.noreply.github.com>
2019-10-15 13:13:44 -0600

    create asp

---
+++ WebApp/BasicAspNet/WebApp/SamplePages/HelloWorld.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="HelloWorld.aspx.cs" Inherits="WebApp.SamplePages.HelloWorld" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Hello World</h1>
+    <asp:Label ID="HelloText" runat="server" Text="Howdy"></asp:Label>
+    <asp:Button ID="Submit" runat="server" Text="Submit" OnClick="Submit_Click" />
+</asp:Content>
---
+++ WebApp/BasicAspNet/WebApp/SamplePages/HelloWorld.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApp.SamplePages
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            HelloText.Text = "Boo!";
+            HelloText.Font.Size = 100;
+            HelloText.ForeColor = System.Drawing.Color.Green;
+        }
+    }
+}
