ea41ead9a9261211299304d9a820d22503429b5c
zdoucette97 <54864223+zdoucette97@users.noreply.github.com>
2019-11-15 13:52:36 -0700

    Update Category.cs

--- CSNet/NorthwindSystem.Data/Category.cs
+++ CSNet/NorthwindSystem.Data/Category.cs
@@ -14,7 +14,8 @@ namespace NorthwindSystem.Data
     [Table("Category")]
     public class Category
     {
-        [Key] //6 and 7 using a view with no table and no key. Looks like an entity but is not one. No table, no key, no dbset)
+        [Key] //6 and 7 using a view with no table and no key. Looks like an entity but is not one. No table, no key, no dbset forCurrentOfferings it is a view)
+        //just create a classs with the. we are filling the view with the ProductController code
         public int? CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
